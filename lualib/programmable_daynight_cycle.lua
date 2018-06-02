e6e2f1a5e19c9e90f313c6120e4b98bebfc8826b
zr4g0n <Dupl3xxx@gmail.com>
2018-06-02 16:43:46 +0200

    Added custom dnc as default

--- lualib/programmable_daynight_cycle.lua
+++ lualib/programmable_daynight_cycle.lua
@@ -1,25 +1,30 @@
 TAU = 6.28318530718 -- this is not the correct form to declare a global variable. Will fix once everything else works
 
 function programmable_daynight_cycle(event)
-	local daylength_ticks = 2000 --The day-night cycle length in ticks. Can be any value.
+	local daylength_ticks = 15000 --The day-night cycle length in ticks. Can be any value.
 	local stepsize_ticks = 59 	 -- warning! stepsize_ticks cannot be over 25000!
 	if not (game.tick % stepsize_ticks == 0) then -- Replace with 'on_nth_tick' once I figure out how to do that
 		return
 	end
 	local time_ratio = (daylength_ticks/25000)
-	game.surfaces[1].daytime = 0
 	local current_time = (game.tick / daylength_ticks)
 	local time_step = (stepsize_ticks/daylength_ticks)
-	game.print("current time: " .. current_time .. " time step " .. time_step)
+	-- game.print("current time: " .. current_time .. " time step " .. time_step)
+	local time1 = current_time
+	local time2 = (current_time + time_step)
+	--local current_curve_start = {x = current_time, y = normalize_dnc(math.sin(time1))}
+	--local current_curve_end = {x = current_time + (time_step * time_ratio), y = normalize_dnc(math.sin(time2))}
 	
-	local current_curve_start = {x = current_time, y = 0.15 + ((math.sin(TAU * current_time)+1)*0.425)}
-	local current_curve_end = {x = current_time + (time_step * time_ratio), y = 0.15 + ((math.sin(TAU * (current_time + time_step))+1)*0.425)}
+	current_curve_start = {x = current_time, y = normalize_dnc(alt_dnc(time1))}
+	current_curve_end = {x = current_time + (time_step * time_ratio), y = normalize_dnc(alt_dnc(time2))}
+	
 	local y_top_start, y_top_end = {x = -999999999, y = 1}, {x = 999999999, y = 1}
 	local y_bot_start, y_bot_end = {x = -999999999, y = 0.15}, {x = 999999999, y = 0.15}
 	
     local top_point = intersection(current_curve_start, current_curve_end, y_top_start, y_top_end)
 	local bot_point = intersection(current_curve_start, current_curve_end, y_bot_start, y_bot_end)
-	-- resetting to assumed safe values before poking further
+	-- cleaning up
+	game.surfaces[1].daytime = 0
 	game.surfaces[1].dusk = -999999999
 	game.surfaces[1].dawn = 999999999
 	game.surfaces[1].evening = -999999998
@@ -31,6 +36,15 @@ function programmable_daynight_cycle(event)
 		game.surfaces[1].morning = bot_point - current_time
 		game.surfaces[1].dawn = top_point - current_time
 	end
+end
+
+function normalize_dnc(n) -- takes [-1, 1] returns [0.15, 1] 
+	return 0.15 + ((n+1)*0.425)
+end
+
+function alt_dnc (x)
+	return (math.sin(x * TAU) + (0.111 * math.sin(3 * x * TAU))) * 1.125
+	-- magic numbers to shape the curve and keep the output to [-1, 1]
 end
 
 -- take all this and put it into a seperate module once everything else works

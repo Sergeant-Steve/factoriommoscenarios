10f7f87c78751f16b201a81497ba48c613d6f5ba
zr4g0n <Dupl3xxx@gmail.com>
2018-08-12 21:18:59 +0200

    PDNC complete rewrite
    More robust, doesn't have features it doesn't need. Right now set up to run the 'doomsday' scenario. Replace pdnc_program() with pdnc_program_basic() in pdnc_core() to disable the doomsday features.

--- control.lua
+++ control.lua
@@ -1,6 +1,7 @@
 -- TOOLS: Recommend all be turned on
 require "lualib/event"
 require "mod-gui" --required for all other modules
+
 require "lualib/topgui" --utility module to be able to order the buttons in the top left
 require "lualib/char_mod"	--utility module to prevent multiple modules conflicting when modifying player bonus
 require "lualib/bot"	--3ra shit
@@ -13,6 +14,8 @@ require "lualib/modular_information/modular_information" --New player informatio
 require "lualib/antigrief" --untested
 require "equipment"
 
+--require "lualib/programmable_daynight_cycle" --Zr's fancy day-night cycle stuff
+require "lualib/pdnc" --Zr's fancy day-night cycle stuff
 -- require "wg_jungle" --Jungle World Generator, generates a world full of trees!
 require "debug"
-require "lualib/programmable_daynight_cycle" --Zr's fancy day-night cycle stuff
+
--- lualib/pdnc.lua
+++ lualib/pdnc.lua
@@ -1,151 +1,112 @@
---global.pdnc = global.programmable_daynight_cycle or {}
---global.pdnc.enabled = global.programmable_daynight_cycle.enabled or true
-global.pdnc_scenario_selection = 0
-global.pdnc_global_darkness = 0
-global.pdnc_global_lightness = 0
-global.pdnc_daylength_ticks = 36000
-global.pdnc_stepsize_ticks = 31 
-global.pdnc_doomsday = false
-global.pdnc_doomsday_timer = 648000 -- 1h27.5m in ticks
-global.current_time = 0
-global.pdnc_disable_warning = true
+-- Third itteration of programmable day-night cycle for Factorio
+-- Developed by Zr4g0n with help
+-- Features needed:
+-- Set 'maximum brightness' from rocket launches cluster wide
+-- Set 'time until doomsday' to any time, in minutes. 
+-- Set 'evolution target' to any number [0.0, 1.0]
+-- Changing day-night cycle based on days
+-- Handle 'next time' elegantly
 
-function pdnc_tick(event)
-	local stepsize_ticks = global.pdnc_stepsize_ticks -- stepsize_ticks < 25000!!!
-	if not ((game.tick % stepsize_ticks == 0) and (global.pdnc.enabled)) then -- Replace with 'on_nth_tick' once I figure out how to do that
-		return
+global.stepsize = 51 -- used for steps as well as on_nth_tick
+global.pdnc_surface = 1
+global.pdnc_current_time = 0
+global.pdnc_current_point = {x = 0, y = 1.0}
+global.pdnc_last_point = {x = -1, y = 0.0}
+global.pdnc_max_brightness = 1.0
+global.pdnc_doomsday_start = 20.5 -- in days
+global.pdnc_pollution_multiplier = 10
+
+script.on_nth_tick(global.stepsize, pdnc_core)
+script.on_init(pdnc_setup)
+script.on_load(pdnc_setup)
+function pdnc_setup()
+    commands.add_command("timeleft", "Gives you the time till doomsday!", pdnc_doomsday_time_left)
+	game.surfaces[s].ticks_per_day = pdnc_min_to_ticks(10)
+end
+
+function pdnc_core()
+	game.print("PDNC core activated")
+	pdnc_freeze_check()
+	local s = global.pdnc_surface
+	global.current_time = game.tick / game.surfaces[s].ticks_per_day
+	global.pdnc_last_point = global.pdnc_current_point
+	global.pdnc_current_point = {x = global.current_time, y = pdnc_program()}
+    local top_point = pdnc_intersection_top (global.pdnc_last_point, global.pdnc_current_point)
+	local bot_point = pdnc_intersection_bot (global.pdnc_last_point, global.pdnc_current_point)
+	
+	if(top_point == bot_point) then
+		game.print("PDNC: Top and bot point equal")
+		-- no cleanup here
+		-- if the points are equal, use last value until not equal
+	elseif(top_point < bot_point) then -- dusk -> evening
+		pdnc_cleanup_last_tick(s) -- placed after, 
+		game.surfaces[s].evening = bot_point - global.current_time
+		game.surfaces[s].dusk = top_point - global.current_time
+	else -- morning -> dawn
+			pdnc_cleanup_last_tick(s)
+		game.surfaces[s].morning = bot_point - global.current_time
+		game.surfaces[s].dawn = top_point - global.current_time
 	end
-	if(global.pdnc_doomsday_timer < game.tick) then
-		local pollution = (global.pdnc_doomsday_timer / game.tick) * global.pdnc_stepsize_ticks
-		local position = {0.0,0.0} -- replace with bilinear aproximation of player position at doomsday
-		game.surfaces[1].pollute(position, (10000 * pollution)) -- arbitrary value
-	end
+end
+
+function pdnc_update_globals(s)
+	game.surfaces[s].daytime = 0
+	game.surfaces[s].dusk = -999999999
+	game.surfaces[s].dawn = 999999999
+	game.surfaces[s].evening = -999999998
+	game.surfaces[s].morning = 999999998
+end
+function pdnc_freeze_check()
 	if(game.surfaces[1].freeze_daytime)then
 		game.surfaces[1].freeze_daytime = false
-		game.print("Can't use freeze_daytime while programmable day-night cycle is active; time has been unfrozen")
-	end
-	local daylength_ticks = global.pdnc_daylength_ticks
-	local current_time = (game.tick / daylength_ticks)
-	global.current_time = (game.tick / daylength_ticks)
-	local next_time = current_time + (stepsize_ticks/daylength_ticks)
-	local curve_start = {x = current_time, y = pdnc_alt_dnc(current_time)}
-	local curve_end = {x = next_time, y = pdnc_alt_dnc(next_time)}
-    local top_point = pdnc_intersection_top(curve_start, curve_end)
-	local bot_point = pdnc_intersection_bot(curve_start, curve_end)
-	-- clean-up and avoiding daytime loop-back
-	game.surfaces[1].daytime = 0
-	game.surfaces[1].dusk = -999999999
-	game.surfaces[1].dawn = 999999999
-	game.surfaces[1].evening = -999999998
-	game.surfaces[1].morning = 999999998
-	-- TODO: add manual logic to check dusk, evening, morning and dawn to avoid errors in the log
-	if(top_point == bot_point)
-		game.print("PDNC top and bot point returned the same value! Ignoring, using last curve")
-	elseif(top_point < bot_point) then -- dusk -> evening
-		game.surfaces[1].evening = bot_point - current_time
-		game.surfaces[1].dusk = top_point - current_time
-	else -- morning -> dawn
-		game.surfaces[1].morning = bot_point - current_time
-		game.surfaces[1].dawn = top_point - current_time
+		game.print("Can't use freeze_daytime while programmable day-night cycle is active; it has been unfrozen")
 	end
 end
 
-function pdnc_alt_dnc(x) -- now more fancy and with 179.9 days 'orbit'
-	if(global.pdnc_scenario_selection = 0)
-		return pdnc_doomsday_scenario(x)
-	elseif(global.pdnc_scenario_selection = 1)
-		return pdnc_unused_scenario(x)
+function pdnc_program()
+	x = global.current_time * math.pi * 2
+	local r = 0
+	if (global.current_time < global.pdnc_doomsday_start) then
+		r = math.pow(pdnc_c_boxy(x), (1 + global.current_time / 4))
+		-- days become shorter over time towards n^6.125
+		
+	elseif (global.current_time < global.pdnc_doomsday_start + 1) then
+		r = math.pow(((global.pdnc_doomsday_start + 1) - global.current_time), 10)
+		pdnc_pollute(r)
 	else
-		return pdnc_default_scenario(x)
+		r = math.pow(pdnc_c_boxy(x), 6.125)*0.5
+	end
+	return pdnc_scaler(r)
+end
+
+function pdnc_c_boxy(x)
+	return pdnc_normalize((math.sin(x) + (0.111 * math.sin(3 * x))) * 1.124859392575928)
+end
+
+function pdnc_pollute(r)
+	local pollution = global.stepsize * r * global.pdnc_pollution_multiplier
+	local position = {x = 0.0, y = 0.0}
+	local step = math.pi / 4
+	for i=0, 7 do
+		position = {x = math.sin(step*i)*256, y = math.cos(step*i)*256}		 
+		game.surfaces[global.pdnc_surface].pollute(position, pollution)
 	end
 end
 
-function pdnc_doomsday_scenario(x)	
-	local current_day = global.current_time * math.pi * 2 --read only!!!
-	local returnvalue = 0
-	local days = x * math.pi * 2
-	local doomsday_start = (global.pdnc_doomsday_timer / global.pdnc_daylength_ticks)
-	if (current_day < doomsday_start) then
-		returnvalue = (1+((math.sin(x) + (0.111 * math.sin(3 * x))) * 1.1225)) * 0.5 -- simpler formula, no 'orbit'
-		return pdnc_range_limiter(returnvalue*0.35 + 0.15)
-	elseif (current_day < doomsday_start + 0.5) then
-		if (global.pdnc_doomsday == false) then
-			game.print("Nuclear winter is here. May god have mercy on your souls...")
-			global.pdnc_doomsday = true
-		end
-		return pdnc_range_limiter((doomsday_start + 1) - current_day)^12)
-	
-	else
-		returnvalue = ((1+(math.sin(x) + (0.111 * math.sin(3 * x)))) * 1.1225) * 0.5 -- simpler formula, no 'orbit'
-		return pdnc_range_limiter((returnvalue * 0.2) - 0.01246)
+function pdnc_normalize(n)
+	return (n + 1)/2
+end
+
+function pdnc_scaler(r)
+	if(r < 0.0) then
+		r = 0.0
 	end
-end
-
-function pdnc_unused_scenario(x)
-	return(pdnc_range_limiter((math.sin(x * math.pi * 2) + 1) * 0.5))
-end
-
-function pdnc_default_scenario(x)	
-	local current_day = global.current_time * math.pi * 2 --read only!!!
-	local returnvalue = 0
-	local days = x
-	x = x * math.pi * 2
-    returnvalue = ((1+(math.sin(x) + (0.111 * math.sin(3 * x)))) * 1.1225) * 0.5 -- simpler formula, no 'orbit'
-	return pdnc_range_limiter((returnvalue * 0.2) - 0.01246)
-end
-
-function pdnc_doomsday_time_left()
-	if(global.pdnc_scenario_selection = 0)
-		local ticks = (global.pdnc_doomsday_timer - game.tick)
-		if (ticks >= 0) then 
-			local seconds = math.floor(ticks/ 60)
-			local minutes = math.floor(seconds / 60)
-			local hours = math.floor(minutes / 60)
-			local days = math.floor(hours / 24)
-			game.print("time until doomsday: " .. string.format("%d:%02d:%02d", hours, minutes % 60, seconds % 60))
-		else
-			ticks = math.abs(ticks) 
-			local seconds = math.floor(ticks / 60)
-			local minutes = math.floor(seconds / 60)
-			local hours = math.floor(minutes / 60)
-			local days = math.floor(hours / 24)
-			game.print("Doomsday was: " .. string.format("%d:%02d:%02d", hours, minutes % 60, seconds % 60) .. " ago...")
-		end
-	else
-		game.print("Error: Doomsday scenraio not currently selected")
-end
-
-function pdnc_scenario_selection(n)
-	global.pdnc_scenario_selection = n
-end
-
-function pdnc_set_doomsday_time_left_ticks(t)
-	global.pdnc_doomsday_time_left = t + game.ticks()
-end
-
-function pdnc_set_doomsday_time_left_minutes(m)
-	global.pdnc_doomsday_time_left = m*60*60 + game.ticks()
-end
-
-function pdnc_set_doomsday_time_left_hours(h)
-	global.pdnc_doomsday_time_left = h*60*60*60 + game.ticks()
-end
-function pdnc_force_doomsday_now()
-	global.pdnc_doomsday_time_left = game.ticks() + global.pdnc_stepsize_ticks  -- makes sure any transitional code runs
-end
-	
-function pdnc_range_limiter(n)
-	if (n < 0) then
-		n = 0
+	if (r > 1.0) then
+		r = 1.0
 	end
-	if (n > 1.0) then
-		n = 1
-	end
-	return 0.15 + global.pdnc_global_lightness + (n * 0.85 * (1 - global.pdnc_global_darkness))
+	return r * (0.85 * global.pdnc_max_brightness) + 0.15
 end
 
--- take all this and put it into a seperate module once everything else works
--- code stolen from https://rosettacode.org/wiki/Find_the_intersection_of_two_lines#Lua
 function pdnc_intersection (s1, e1, s2, e2)
   local d = (s1.x - e1.x) * (s2.y - e2.y) - (s1.y - e1.y) * (s2.x - e2.x)
   local a = s1.x * e1.y - s1.y * e1.x
@@ -165,62 +126,42 @@ function pdnc_intersection_bot (s2, e2)
 	return pdnc_intersection (s1, e1, s2, e2)
 end
 
-
-function pdnc_global_darkness(n)
-  if((n>=0)and(n=<1.0))
-	global.pdnc_global_darkness = n
-  else
-    game.print("global darkness needs to be [0.0 - 1.0], was: " .. n)
-  end
-end
-
-function pdnc_disable()
-	if(pdnc_disable_warning)
-		game.print("Repeat the command to disable Programmable Day-Night Cycle. This cannot be undone.")
-		pdnc_disable_warning = false
-	else
-		pdnc_force_disable()
-	end
-end
-
-function pdnc_force_disable()
-	global.pdnc.enabled = false
-	Event.remove(defines.events.on_tick, pdnc_tick)
-	game.surfaces[1].daytime = 0
-	game.surfaces[1].dusk = -999999999
-	game.surfaces[1].dawn = 999999999
-	game.surfaces[1].evening = -999999998
-	game.surfaces[1].morning = 999999998
-	-- first setting to safe values before setting them back to defaults. 
-	game.surfaces[1].evening = 0.45
-	game.surfaces[1].morning = 0.55
-	game.surfaces[1].dusk = 0.25
-	game.surfaces[1].dawn = 0.75
-	game.print("Resetting day-night cycle to default values")
-end
-
-function pdnc_stepsize_ticks(n)
-	if ((n < 24998) and (n >= 1)) then -- 2 tick margin 
-		global.pdnc_stepsize_ticks = n
-	else 
-		game.print("pdnc_stepsize_ticks was set to " .. n .. " but needs to be [1, 24998)")
-		n = 59 -- reasonably good value for most uses. 
-	end
-end
-
-function pdnc_daylength_ticks(n)
-	if ((n < 1) or (n < global.pdnc_stepsize_ticks)) then
-		game.print("tried to set global.pdnc_daylength_ticks to an unreasonable value: " .. n)
-		global.pdnc_daylength_ticks = 36000 -- 10min default
-	else 
-		global.pdnc_daylength_ticks = n
-	end
-end
-
-function pdnc_init()
+function pdnc_setup()
     commands.add_command("timeleft", "Gives you the time till doomsday!", pdnc_doomsday_time_left)
 end
 
--- seems the error with the command not working on reload is related to this part. 
-Event.register(defines.events.on_tick,pdnc_tick)
-Event.register(-1, pdnc_init)
+function pdnc_set_max_brightness(n)
+	if(n < 0) then
+		game.print("tried to set max brightness to " .. n .. " needs to be between 0.0 and 1.0")
+		n = 0
+	end
+	if(n > 1) then
+		game.print("tried to set max brightness to " .. n .. " needs to be between 0.0 and 1.0")
+		n = 1
+	end
+	global.pdnc_max_brightness = n
+		game.print("global.pdnc_max_brightness set to " .. global.pdnc_max_brightness)
+end
+
+function pdnc_min_to_ticks(m)
+	return 60*60*m
+end
+
+function pdnc_doomsday_time_left()
+	local ticks_until_doomsday = game.surfaces[global.pdnc_surface].ticks_per_day * global.pdnc_doomsday_start
+	local ticks = ticks_until_doomsday - game.tick
+	if (ticks >= 0) then 
+		local seconds = math.floor(ticks/ 60)
+		local minutes = math.floor(seconds / 60)
+		local hours = math.floor(minutes / 60)
+		local days = math.floor(hours / 24)
+		game.print("time until doomsday: " .. string.format("%d:%02d:%02d", hours, minutes % 60, seconds % 60))
+	else
+		ticks = ticks * -1 
+		local seconds = math.floor(ticks / 60)
+		local minutes = math.floor(seconds / 60)
+		local hours = math.floor(minutes / 60)
+		local days = math.floor(hours / 24)
+		game.print("Doomsday was: " .. string.format("%d:%02d:%02d", hours, minutes % 60, seconds % 60) .. " ago...")
+	end
+end

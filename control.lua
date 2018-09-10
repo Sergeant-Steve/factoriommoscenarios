3f7c735c6c468e495b72f0184d4c9757749ef1d4
Lennard Röttjers <Lennard@rottjers.nl>
2018-09-11 00:08:57 +0200

    Enabled Announcements by default

--- control.lua
+++ control.lua
@@ -7,7 +7,7 @@ require "mod-gui" --required for all other modules
 require "lualib/topgui" --utility module to be able to order the buttons in the top left
 require "lualib/char_mod"	--utility module to prevent multiple modules conflicting when modifying player bonus
 require "lualib/bot"	--3ra shit
---require "announcements"	--Module to announce stuff ingame / give the players a welcome message
+require "announcements"	--Module to announce stuff ingame / give the players a welcome message
 --require "rocket" --Module to stop people removing the rocket silo
 --require "gravemarker" --Create a map tag on player death for easier corpse finding
 require "lualib/modular_tag/modular_tag" --Module to let players set a tag behind their names to improve teamwork, also allows other modules to get (and use) its canvas.
